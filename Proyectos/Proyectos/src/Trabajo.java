c4171751cb74161e7b682903a51f3dec756e18ba
Manuel Ruz <manuel22ene@gmail.com>
2020-11-12 17:25:53 +0100

    segundo punto

--- Proyectos/Proyectos/src/Trabajo.java
+++ Proyectos/Proyectos/src/Trabajo.java
@@ -4,5 +4,6 @@ public class Trabajo {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("que tal");
+        System.out.println("bien");
     }
 }

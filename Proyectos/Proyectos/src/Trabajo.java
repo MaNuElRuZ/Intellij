f0713ea67fc12068c1e0cc88c1a6db753a8af621
Manuel Ruz <manuel22ene@gmail.com>
2020-11-12 17:49:24 +0100

    estamos vivos

--- Proyectos/Proyectos/src/Trabajo.java
+++ Proyectos/Proyectos/src/Trabajo.java
@@ -4,5 +4,7 @@ public class Trabajo {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("que tal");
+        System.out.println("bien");
+        System.out.println("estamos vivos");
     }
 }
